dbcf862b85b38fd32a28ae2ee96d49978970cc39
GauravSinghdev <gauravkalakoti19@gmail.com>
2025-06-26 08:45:47 +0530

    adding sitemap.ts file

---
+++ app/sitemap.ts
@@ -0,0 +1,10 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://projects.supxdevs.com",
+      lastModified: "2025-06-26",
+    },
+  ];
+}
